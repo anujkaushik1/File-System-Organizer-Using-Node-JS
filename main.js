00637707fe40b00c566f5ef3f5557e0e098b68c5
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-06-07 12:24:39 +0530

    Implemented getCategory func for getting extentions of all files and matching with object

--- main.js
+++ main.js
@@ -3,11 +3,25 @@ let command = inputArr[0];
 let fs = require("fs");
 let path = require("path");
 let types = {
-  media : ['mp4, mkv'],
-  archives : ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', 'xz'],
-  documents : ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
-  app : ['exe', 'dmg', 'pkg', 'deb']
-}
+  media: ["mp4, mkv"],
+  archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+  documents: [
+    "docx",
+    "doc",
+    "pdf",
+    "xlsx",
+    "xls",
+    "odt",
+    "ods",
+    "odp",
+    "odg",
+    "odf",
+    "txt",
+    "ps",
+    "tex",
+  ],
+  app: ["exe", "dmg", "pkg", "deb"],
+};
 
 switch (command) {
   case "tree":
@@ -51,9 +65,26 @@ function organizeHelper(src, dest) {
     let childAddress = path.join(src, childNames[i]);
     let isFile = fs.lstatSync(childAddress).isFile();
     if (isFile) {
-      console.log(childNames[i]);
+      let category = getCategory(childNames[i]);
+      console.log(childNames[i], 'belongs to', category);
     }
   }
+}
+
+function getCategory(name) {
+  let ext = path.extname(name);
+  ext = ext.slice(1);
+
+  for (let type in types) {   // will get all keys from object(media,archives,documents)
+    let currTypeArr = types[type];
+    for (let i = 0; i < currTypeArr.length; i++) {
+      if (ext == currTypeArr[i]) {
+        return type;
+      }
+    }
+  }
+
+  return "others";
 }
 
 function helpFun(dirPath) {
